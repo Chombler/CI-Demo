9cacb72a458007ceda666bb274fae6aaa0f922a9
XavierJCallait <113396487+XavierJCallait@users.noreply.github.com>
2023-09-12 14:21:48 -0400

    Update secret.ts
    Add functions

--- src/secret.ts
+++ src/secret.ts
@@ -12,8 +12,8 @@
 // 5, 2 -> 1
 // 7, 4 -> 3
 // All inputs must be >= 1
-export function secret_1(_num1: number, _num2: number): number {
-    return 0;
+export function secret_1(_num1: number, _num2: number): number { // modulus
+    return num1 % num2;
 }
 
 // Implements the following input to output mapping:
@@ -22,8 +22,8 @@ export function secret_1(_num1: number, _num2: number): number {
 // 5, 2 -> 25
 // 7, 4 -> 2401
 // All inputs must be >= 1
-export function secret_2(_num1: number, _num2: number): number {
-    return 0;
+export function secret_2(_num1: number, _num2: number): number { // power
+    return pow(num1, num2);
 }
 
 // Implements the following input to output mapping:
@@ -32,8 +32,8 @@ export function secret_2(_num1: number, _num2: number): number {
 // 9 -> 3
 // 256 -> 16
 // All inputs must be >= 0
-export function secret_3(_num1: number): number {
-    return 0;
+export function secret_3(_num1: number): number { // sqrt
+    return sqrt(num1);
 }
 
 // Implements the following input to output mapping:
@@ -42,8 +42,11 @@ export function secret_3(_num1: number): number {
 // 5 -> 120
 // 7 -> 5040
 // All inputs must be >= 0
-export function secret_4(_num1: number): number {
-    return 0;
+export function secret_4(_num1: number): number { // factorial
+    if (num1 <= 1) {
+        return 1;
+    }
+    return secret_4(num1 - 1) * num1;
 }
 
 // Implements the following input to output mapping:
@@ -52,6 +55,9 @@ export function secret_4(_num1: number): number {
 // 5 -> 5
 // 7 -> 13
 // All inputs must be >= 1
-export function secret_5(_num1: number): number {
-    return 0;
+export function secret_5(_num1: number): number { // fibonacci
+    if (num1 <= 1) {
+        return 1;
+    }
+    return secret_5(num1 - 2) + secret_5(num2 - 1);
 }
