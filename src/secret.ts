17de0378a388330048fa4b64f31b5e58abac1baf
Calix-Mingyu-Kim <66811344+Calix-Mingyu-Kim@users.noreply.github.com>
2023-09-12 14:25:10 -0400

    Update secret.ts

--- src/secret.ts
+++ src/secret.ts
@@ -13,7 +13,7 @@
 // 7, 4 -> 3
 // All inputs must be >= 1
 export function secret_1(_num1: number, _num2: number): number {
-    return 0;
+    return _num1 % _num2;
 }
 
 // Implements the following input to output mapping:
@@ -23,7 +23,7 @@ export function secret_1(_num1: number, _num2: number): number {
 // 7, 4 -> 2401
 // All inputs must be >= 1
 export function secret_2(_num1: number, _num2: number): number {
-    return 0;
+    return  _num1 ** _num2;
 }
 
 // Implements the following input to output mapping:
@@ -33,7 +33,7 @@ export function secret_2(_num1: number, _num2: number): number {
 // 256 -> 16
 // All inputs must be >= 0
 export function secret_3(_num1: number): number {
-    return 0;
+    return _num1 ** (1/2);
 }
 
 // Implements the following input to output mapping:
@@ -43,7 +43,11 @@ export function secret_3(_num1: number): number {
 // 7 -> 5040
 // All inputs must be >= 0
 export function secret_4(_num1: number): number {
-    return 0;
+    ans: number = 1
+    for (let i=_num1; i > 0; i--) {
+        ans = ans * i
+    }
+    return ans;
 }
 
 // Implements the following input to output mapping:
@@ -53,5 +57,16 @@ export function secret_4(_num1: number): number {
 // 7 -> 13
 // All inputs must be >= 1
 export function secret_5(_num1: number): number {
+    if (_num1 == 0) return 0;
+    else if (_num1 == 1) return 1;
+    else {
+        prev1 = 0
+        prev2 = 1
+        for (let i=2; i<_num1; i++) {
+            let temp = prev2
+            prev2 += prev1
+            prev1 = temp
+        }
+    }
     return 0;
 }
