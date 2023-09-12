e53a69792b6377ab9a604e171bbc5be15cb8cd2a
mghera02 <mghera02@hotmail.com>
2023-09-12 14:26:27 -0400

    added secret

--- src/secret.ts
+++ src/secret.ts
@@ -13,7 +13,7 @@
 // 7, 4 -> 3
 // All inputs must be >= 1
 export function secret_1(_num1: number, _num2: number): number {
-    return 0;
+    return _num1 % _num2;
 }
 
 // Implements the following input to output mapping:
@@ -23,7 +23,7 @@ export function secret_1(_num1: number, _num2: number): number {
 // 7, 4 -> 2401
 // All inputs must be >= 1
 export function secret_2(_num1: number, _num2: number): number {
-    return 0;
+    return Math.pow(_num1, _num2);
 }
 
 // Implements the following input to output mapping:
@@ -33,7 +33,7 @@ export function secret_2(_num1: number, _num2: number): number {
 // 256 -> 16
 // All inputs must be >= 0
 export function secret_3(_num1: number): number {
-    return 0;
+    return Math.sqrt(_num1);
 }
 
 // Implements the following input to output mapping:
@@ -43,7 +43,11 @@ export function secret_3(_num1: number): number {
 // 7 -> 5040
 // All inputs must be >= 0
 export function secret_4(_num1: number): number {
-    return 0;
+    let out = 1;
+    for(let i = _num1; i > 1; i--) {
+        out *= i;
+    }
+    return out;
 }
 
 // Implements the following input to output mapping:
@@ -53,5 +57,23 @@ export function secret_4(_num1: number): number {
 // 7 -> 13
 // All inputs must be >= 1
 export function secret_5(_num1: number): number {
-    return 0;
+    let n = _num1;
+    if (n <= 0) {
+        throw new Error("Input must be a positive integer");
+      }
+    
+      if (n === 1 || n === 2) {
+        return 1;
+      }
+    
+      let prev = 1;
+      let current = 1;
+    
+      for (let i = 3; i <= n; i++) {
+        const next = prev + current;
+        prev = current;
+        current = next;
+      }
+    
+      return current;
 }
